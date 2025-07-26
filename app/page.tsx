4c93ec5134a021055e0f8fb0d535daa680842b7b
Razan <razan.rezzq@gmail.com>
2025-07-26 18:07:50 +0300

    chore: add initial Tailwind CSS configuration file

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl">
-        Welcome to Next.js! Welcome to Next.js! Welcome to Next.js! Welcome to
-        Next.js! Welcome to Next.js! Welcome to Next.js! Welcome to Next.js!
+      <h1 className="h1-bold">Welcome to Next.js!</h1>
+      <h1 className="h1-bold space-grotesk">
+        Welcome to Next.js! (space-grotesk)
       </h1>
-      <Image
-        src="/nextjs-logo.png"
-        alt="Next.js Logo"
-        width={500}
-        height={500}
-      />
+      <h2 className="h2-regular inter">
+        Learn how to build your Next.js application
+      </h2>
     </main>
   );
 }
