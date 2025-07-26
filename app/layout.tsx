1d45af823089b018b30f64f4146d7eb28527d17e
Razan <razan.rezzq@gmail.com>
2025-07-26 11:13:47 +0300

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono as GeistMono } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
 const geistSans = Geist({
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Next.js!</h1>
+      <h1 className="text-3xl">
+        Welcome to Next.js! Welcome to Next.js! Welcome to Next.js! Welcome to
+        Next.js! Welcome to Next.js! Welcome to Next.js! Welcome to Next.js!
+      </h1>
       <Image
         src="/nextjs-logo.png"
         alt="Next.js Logo"
