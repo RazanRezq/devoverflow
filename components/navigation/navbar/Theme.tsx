1c1fa3f8644448f58beee4fb92a334ed19ef6346
Razan <razan.rezzq@gmail.com>
2025-08-03 13:57:05 +0300

    feat: implement Theme component with dropdown for theme selection and add Button component

---
+++ components/navigation/navbar/Theme.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Theme = () => {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun
+            className={`h-[1.2rem] w-[1.2rem] transition-all ${
+              resolvedTheme === "dark"
+                ? "scale-0 -rotate-90"
+                : "scale-100 rotate-0"
+            }`}
+          />
+          <Moon
+            className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+              resolvedTheme === "dark"
+                ? "scale-100 rotate-0"
+                : "scale-0 rotate-90"
+            }`}
+          />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Theme;
